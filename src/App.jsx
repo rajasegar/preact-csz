42024964adac8221ff2e4f02f143affc977bf09e
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-06-22 08:19:53 +0530

    Add csz for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
 import { h } from 'preact';
 import logo from './logo.png';
-import './App.css';
+import csz from 'csz';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={csz`text-align:center;`}>
+      <header className={csz`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+      `}>
+        <img src={logo} className={csz`
+
+  height: 40vmin;
+  pointer-events: none;
+  margin-bottom: 1rem;
+  animation: App-logo-spin infinite 1.6s ease-in-out alternate;
+
+
+@keyframes App-logo-spin {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(1.06);
+  }
+}
+        `} alt="logo" />
         <p>
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={csz` color: #61dafb; `}
           href="https://preactjs.com"
           target="_blank"
           rel="noopener noreferrer"
